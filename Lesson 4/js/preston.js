49886fff1ad5a8155f2d5d4d37ad640dbc1f23ee
gillianbien <gillianbienvinuto@gmail.com>
2021-05-13 18:38:20 +0800

    commit

--- Lesson 4/js/preston.js
+++ Lesson 4/js/preston.js
@@ -5,7 +5,7 @@ function toggleMenu(){
 var today = new Date();
 var day=today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var month=(today.getMonth()+1);
+var month=today.getMonth();
 var monthlist = ["January","February","March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var date = daylist[day]+','+ today.getDate()+' '+monthlist[month] +' '+today.getFullYear();
 document.getElementById('currentDate').innerHTML= date;
