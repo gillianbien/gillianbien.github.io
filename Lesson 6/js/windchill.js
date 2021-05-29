32ce5b82eeb81e67629c1fbd85aec4b867c99dc4
gillianbien <gillianbienvinuto@gmail.com>
2021-05-29 12:44:02 +0800

    commit

--- Lesson 6/js/windchill.js
+++ Lesson 6/js/windchill.js
@@ -1,10 +1,8 @@
- //funtions for input and output
- function doInputOutput(){
     let t= parseFloat(document.getElementById('inputTemp').value);
     let s= parseFloat(document.getElementById('inputWind').value);
     let answer=windChill(t, s);
-    document.getElementById('outputspan').innerHTML="Answer: " + answer;
- }
+    document.getElementById('outputspan').innerHTML= answer;
+
  //function for the formula
  function windChill(tempF, speed){
      let f=(35.74 + (0.6215 * tempF)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16)));
