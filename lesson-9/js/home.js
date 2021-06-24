6a4fb2444cf30f99642d0b32d5e5b3c1775e3119
gillianbien <gillianbienvinuto@gmail.com>
2021-06-25 07:19:40 +0800

    commit

--- lesson-9/js/home.js
+++ lesson-9/js/home.js
@@ -6,15 +6,20 @@ fetch(requestURL)
 .then(function (jsonObject) {
   console.table(jsonObject);
   const towns = jsonObject['towns'];
-  for (let i = 0; i < towns.length; i++ ) { 
+  for (let i = 0; i < towns.length; i+=2 ) {
+    if(i==!4) continue;
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let yearFounded = document.createElement('h4');
     let population = document.createElement('h4');
     let annualRF =document.createElement('h4')
     let image = document.createElement('img');
-    image.setAttribute('src', towns[i].photo);
-    
+    if(i===0){
+    image.setAttribute('src', "images/sodasprings.jpg");}
+    if(i===2){
+    image.setAttribute('src', "images/fishhaven.jpg");}
+    if(i===6){
+    image.setAttribute('src', "images/preston.jpg");}
     
     h2.textContent = towns[i].name;
     yearFounded.textContent = "Year Founded: " + ' ' + towns[i].yearFounded;
@@ -26,7 +31,9 @@ fetch(requestURL)
     card.appendChild(annualRF);
     card.appendChild(image);
     
+    
     document.querySelector('div.cards').appendChild(card); 
-    }// temporary checking for valid response and data parsing
+  }
+  // temporary checking for valid response and data parsing
   });
   
