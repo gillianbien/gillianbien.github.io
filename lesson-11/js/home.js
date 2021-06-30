d3ebc3a5db812e68ea6d66f662ef5a17d85b2f0d
gillianbien <gillianbienvinuto@gmail.com>
2021-06-30 14:53:27 +0800

    commit

---
+++ lesson-11/js/home.js
@@ -0,0 +1,41 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (jsonObject) {
+  console.table(jsonObject);
+  const towns = jsonObject['towns'];
+  for (let i = 0; i < towns.length; i+=2 ) {
+    if(i==!4)continue;
+    let card = document.createElement('section');
+    let div = document.createElement('div');
+    let h2 = document.createElement('h2');
+    let yearFounded = document.createElement('h4');
+    let population = document.createElement('h4');
+    let annualRF =document.createElement('h4')
+    let image = document.createElement('img');
+    if(i===4){
+    image.setAttribute('src', "images/sodasprings.jpg");}
+    if(i===2){
+    image.setAttribute('src', "images/fishhaven.jpg");}
+    if(i===6){
+    image.setAttribute('src', "images/preston.jpg");}
+    
+    h2.textContent = towns[i].name;
+    yearFounded.textContent = "Year Founded: " + ' ' + towns[i].yearFounded;
+    population.textContent = "Population: " + ' ' + towns[i].currentPopulation;
+    annualRF.textContent ="Annual Rain Fall: " + ' ' + towns[i].averageRainfall;
+    card.appendChild(div);
+    div.appendChild(h2);
+    div.appendChild(yearFounded);
+    div.appendChild(population);
+    div.appendChild(annualRF);
+    card.appendChild(image);
+    
+    
+    document.querySelector('div.cards').appendChild(card); 
+  }
+  // temporary checking for valid response and data parsing
+});
+  
---
+++ lesson-11/js/windchill.js
@@ -0,0 +1,11 @@
+    let t= parseFloat(document.getElementById('inputTemp').value);
+    let s= parseFloat(document.getElementById('inputWind').value);
+    let answer=windChill(t, s);
+    document.getElementById('outputspan').innerHTML= answer;
+
+ //function for the formula
+ function windChill(tempF, speed){
+     let f=(35.74 + (0.6215 * tempF)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16)));
+     return f;
+
+ }
